6edc77ba4f71cb8a4adcf267d3cafccd0f2f08bb
Marcelo Luis <marceloluis257gh@gmail.com>
2022-03-31 18:33:12 -0300

    subindo forma basica, ninguem vai ver isso, me da um desconto

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,78 @@
-use std::process::Command;
-use utils::{input, pause, logo, clear_scream};
+extern crate ncurses;
+use ncurses::*;
 
+const DESSELECIONADO: i16 = 0; 
+const SELECIONADO: i16 = 1;
+
+pub struct Projetos {
+    inacabados: Vec<String>,
+    feitos: Vec<String>,
+}
 
 fn main() {
-    
-    let mut projetos = vec![
-        "Orogonize".to_string(),
-        "EScolheProjeto".to_string(),
-        "MicroRPG".to_string()
-    ];
+    initscr();                                     // Inicia o terminal
+    start_color();                                 // Ativa formatação de cores 
+    noecho();                                      // Desativa o echo
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE); // Desativa o cursos
 
-    loop {
-        clear_scream();
-        logo();
+    init_pair(DESSELECIONADO, COLOR_WHITE, COLOR_BLACK); // Seta a formação para o item desselecionado 
+    init_pair(SELECIONADO, COLOR_BLACK, COLOR_WHITE);    // Seta a formação para o item selecionado 
 
-        for (count, i) in projetos.iter().enumerate() {
-            println!(" {}º Projeto - {}", count + 1, i);
+    // Seta as primeiras 4 linhas que n serão alteradass
+    addstr("\n");
+    addstr(" --------- Projetos ----------- \n");
+    addstr(" Qual projeto você desejá abrir? \n");
+    addstr("\n");
+
+    // Gera a Struct com os projetos
+    let mut p = Projetos{
+        inacabados: vec![
+            "Orgonize".to_string(),
+            "MicroRPG".to_string(),
+            "EscolheProjeto".to_string(),
+        ],
+        feitos: Vec::<String>::new(),
+    };
+
+    let mut quit = false;
+    let mut item_selecioado = 0; // Cursos para destacar o item selecionado
+    while !quit {
+        // Redenriza os projetos inacamados na tela
+        for (row, item) in p.inacabados.iter().enumerate() {
+            // Verifica es esta selecionado
+            let pair = if item_selecioado == row { 
+                SELECIONADO 
+            } else { 
+                DESSELECIONADO 
+            };
+            
+            attron(COLOR_PAIR(pair)); // Seta o estilo de formatação
+            
+            // Move para a posição correta e adiciona o projeto ao buffer
+            mv((row + 4) as i32, 3); 
+            addstr(&format!(" {} - [ ] {} ", row + 1, item)); 
+            
+            attroff(COLOR_PAIR(pair)); // Desabilida a formatação
         }
+        refresh();
 
-        pause();
+        // Le a entrada do usuario
+        // 'q' -> Sai do programa
+        // Arrow Up e Down movem o cursor
+        let key = getch(); 
+        match key as u8 as char { 
+            'q' => quit = true,
+            // A por algum motivo tem o mesmo codigo de Arrow Up
+            'A' => if item_selecioado >= 1 { 
+                item_selecioado -= 1 
+            },
+            // B por algum motivo tem o mesmo codigo de Arrow Down
+            'B' => if item_selecioado < p.inacabados.len() - 1 { 
+                item_selecioado += 1
+            }, 
+            _ => {},
+        }
     }
+
+    endwin();
 }
