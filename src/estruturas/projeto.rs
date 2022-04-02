35d1112a6c2223b9363deba59539c36b3f173af8
Marcelo Luis <marceloluis257gh@gmail.com>
2022-04-02 17:47:26 -0300

    Desdici apagar tudo do main e começar de novo

--- src/estruturas/projeto.rs
+++ src/estruturas/projeto.rs
@@ -1,13 +1,12 @@
 use std::io::{BufRead, BufReader};
 mod arquivo;
 
-
-pub fn carrega_projetos(path: &str) -> Vec<(String, bool)> {
+pub fn carrega_projetos(path: &str) -> Vec<Vec<String>> {
     let mut inacabados = Vec::new();
     let arquivo = BufReader::new(arquivo::carrega_arquivo(path));
     for line in arquivo.lines() {
         match line {
-            Ok(line) => inacabados.push((line, false)),
+            Ok(line) => inacabados.push(vec![line, "0".to_string()]),
             Err(err) => {
                 panic!("Erro ao tentar ler arquivo | Error {}", err)
             }
