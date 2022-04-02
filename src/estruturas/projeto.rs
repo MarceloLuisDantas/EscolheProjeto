22981d4f1625abbe2c41de3969f90e795be8bf7a
Marcelo Luis <marceloluis257gh@gmail.com>
2022-04-02 18:31:21 -0300

    Decidi voltar a usar objetos para facilitar as abstração

--- src/estruturas/projeto.rs
+++ src/estruturas/projeto.rs
@@ -1,17 +1,47 @@
 use std::io::{BufRead, BufReader};
 mod arquivo;
 
-pub fn carrega_projetos(path: &str) -> Vec<Vec<String>> {
-    let mut inacabados = Vec::new();
-    let arquivo = BufReader::new(arquivo::carrega_arquivo(path));
-    for line in arquivo.lines() {
-        match line {
-            Ok(line) => inacabados.push(vec![line, "0".to_string()]),
-            Err(err) => {
-                panic!("Erro ao tentar ler arquivo | Error {}", err)
-            }
-        }            
-    }
-    return inacabados
+pub struct Projeto {
+    nome: String,
+    path: String,
+    finalizado: bool,
 }
 
+impl Projeto {
+    pub fn show(&self) {
+        println!("{} | {} | {}", self.nome, self.path, self.finalizado);
+    }
+    pub fn alterar_estados(&mut self) {
+        self.finalizado = !self.finalizado;
+    }
+}
+
+#[derive(Default)]
+pub struct Projetos {
+    pub lista: Vec<Projeto>,
+}
+
+impl Projetos {
+    pub fn carrega_projetos(&mut self) {
+        let arquivo = BufReader::new(arquivo::carrega_arquivo("./projetos.txt"));
+        for line in arquivo.lines() {
+            match line {
+                Ok(line) => {
+                    let mut valores = Vec::new();
+                    for i in line.split("|") {
+                        valores.push(i);
+                    }             
+                    let p = Projeto {
+                        nome: valores[0].trim().to_string(),
+                        path: valores[1].trim().to_string(),
+                        finalizado: valores[2].trim() == "finalizado",
+                    };
+                    self.lista.push(p);
+                },
+                Err(err) => {
+                    panic!("Erro ao tentar ler arquivo | Error {}", err)
+                }
+            } 
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 #![allow(non_snake_case)]
 
-fn main() {
+#[path="./estruturas/projeto.rs"]
+mod projeto;
 
+fn main() {
+    let mut p: projeto::Projetos = Default::default() ;
+    p.carrega_projetos();
+    for i in p.lista.iter() {
+        i.show();
+    }
 }
